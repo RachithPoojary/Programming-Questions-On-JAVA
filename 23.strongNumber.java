f3f81ccd02c18c50335228f69318a36ac2052baa
Rachith Poojary <rachithstudy@gmail.com>
2024-08-23 10:43:00 +0530

    23-08-24

---
+++ 23.strongNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class strongNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0,temp = n;
+        while (n!=0){
+            int digit = n%10;
+            int fact = 1;
+            for (int i = 1; i<=digit;i++){
+                fact *= i;
+            }
+            sum += fact;
+            n/=10;
+        }
+        if (temp == sum){
+            System.out.println("Strong Number");
+        }else{
+            System.out.println("Not a Strong Number");
+        }
+    }
+}
---
+++ 25.NumberOfDigit.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class NumberOfDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int count = 0;
+        while(num>0){
+            num /= 10;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
