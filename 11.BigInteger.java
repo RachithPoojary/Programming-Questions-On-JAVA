c1f00e9a1714f45af182652c4cb1b2b2c5ea4c61
Rachith Poojary <rachithstudy@gmail.com>
2024-08-24 23:27:56 +0530

    24-08

--- 11.BigInteger.java
+++ 11.BigInteger.java
@@ -4,7 +4,7 @@ class  usingBigInteger
 {
     public static void main(String[] args)
     {
-        int n=100;
+        int n=5;
         BigInteger fact= BigInteger.valueOf(1);
         System.out.println(fact);
         for(int i=1;i<=n;i++)
