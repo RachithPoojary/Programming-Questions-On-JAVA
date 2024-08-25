e790b2b303b0994b275fc702e6e12456256b0862
Rachith Poojary <rachithstudy@gmail.com>
2024-08-25 10:07:29 +0530

    25-08-24 :-10:07

---
+++ 27.strongNumberInARange.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class strongNumberInARange {
+    public static void main(String[] args) {
+        for (int n = 1;n<=1000;n++){
+            if (isStrongNumber(n)){
+                System.out.println(n);
+            }
+        }
+    }
+        public static boolean isStrongNumber(int n){
+            int sum = 0,temp = n;
+            while (n!=0){
+                int digit = n%10;
+                int fact = 1;
+                for (int i = 1; i<=digit;i++){
+                    fact *= i;
+                }
+                sum += fact;
+                n/=10;
+            }
+            return temp == sum;
+    }
+}
---
+++ 28.perfectNumberInARange.java
@@ -0,0 +1,19 @@
+class perfectNumberInARange {
+    public static void main(String[] args) {
+        for (int n = 1;n<=1000;n++){
+            if(isPerfectNumber(n)){
+                System.out.println(n);
+            }
+        }
+
+    }
+    public static boolean isPerfectNumber(int n){
+        int sum = 0;
+        for (int i = 1;i<=n/2;i++){
+            if(n%i==0){
+                sum += i;
+            }
+        }
+        return sum == n;
+    }
+}
