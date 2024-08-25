7e766b9e5889458ca442edc481ec8b3cfe73991f
Rachith Poojary <rachithstudy@gmail.com>
2024-08-25 10:46:57 +0530

    25-08-24 :- 10:46

---
+++ 29.decimalToBinary.java
@@ -0,0 +1,18 @@
+//WAP TO CONVERT THE DECIMAL VALUE INTO BINARY
+
+import java.util.Scanner;
+
+class decimalToBinary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int decimal = sc.nextInt();
+        String binary = "";
+        while (decimal>0){
+            int remainder = decimal % 2;
+            binary = remainder + binary;
+
+            decimal /=2;
+        }
+        System.out.println(binary);
+    }
+}
